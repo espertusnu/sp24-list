b8c074f1dbaab097402b05b1703591b24381a08c
Ellen Spertus <e.spertus@northeastern.edu>
2024-02-05 10:51:05 -0800

    Improve javadoc

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -18,7 +18,7 @@ public interface List<T> {
     /**
      * Adds the given item at the end of this list.
      *
-     * @param s the item to add
+     * @param item the item to add
      */
-    public void add(T s);
+    public void add(T item);
 }
